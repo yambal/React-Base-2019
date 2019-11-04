b4405f9d900d3aba19509c65db312b2691f9925f
June YAMAMOTO <june@yambal.net>
2019-11-04 23:21:15 +0900

    Font

--- src/container/PdfMaker.tsx
+++ src/container/PdfMaker.tsx
@@ -11,16 +11,15 @@ var pdfMake = require('pdfmake/build/pdfmake.js');
  * \node_modules\pdfmake で gulp buildFonts
  * \node_modules\pdfmake\build\vfs_fonts.js が生成される
  */
-var pdfFonts = require('./vfs_NotoSansCJKjp_2_fonts.js');
+
+var pdfFonts = require('./vfs_FlopDesignFONT_fonts.js');
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 pdfMake.fonts = {
-  NotoSans: {
-    normal: 'NotoSansCJKjp-Regular.otf',
-    bold: 'NotoSansCJKjp-Bold.otf',
-    italics: 'NotoSansCJKjp-Thin.otf',
-    bolditalics: 'NotoSansCJKjp-Black.otf'
+  FlopDesignFONT: {
+    normal: 'FlopDesignFONT.otf'
   },
 };
+
 const PdfMaker:React.FC = () => {
   const [url, setUrl] = React.useState('')
 
@@ -28,19 +27,62 @@ const PdfMaker:React.FC = () => {
     a()
   }, []);
 
+  const w = 780
+  const h = 530
+
   const a = () => {
     
     var win = window.open('', '_blank');
     pdfMake.createPdf({
         content: [
-          {text: '1 Headline', headlineLevel: 1},
-          'あいうえおかきくけこさしすせそ',
-          '別の段落、今回はのためにもう少し長く、この行は少なくとも2行にされます別の段落',
-          '別の段落、今回はのためにもう少し長く、この行は少なくとも2行にされます'
+          { svg: `<svg width="780" height="530" font-family="FlopDesignFONT" font-size="8">
+              <g width="${w}" height="${h}" >
+                <g transform="translate(${w / 4},${h / 2}) rotate(180)">
+                  <rect x="0" y="0" width="${w / 4}" height="${h / 2}" stroke="lightgray" stroke-width="1" fill="none"/>
+                  <text x="0" y="12">1 本日は晴天なり</text>
+                </g>
+                <g transform="translate(${w / 2},${h / 2}) rotate(180)">
+                  <rect x="0" y="0" width="${w / 4}" height="${h / 2}" stroke="lightgray" stroke-width="1" fill="none"/>
+                  <text x="0" y="12">2</text>
+                </g>
+                <g transform="translate(${w / 4 * 3},${h / 2}) rotate(180)">
+                  <rect x="0" y="0" width="${w / 4}" height="${h / 2}" stroke="lightgray" stroke-width="1" fill="none"/>
+                  <text x="0" y="12">3</text>
+                </g>
+                <g transform="translate(${w / 4 * 4},${h / 2}) rotate(180)">
+                  <rect x="0" y="0" width="${w / 4}" height="${h / 2}" stroke="lightgray" stroke-width="1" fill="none"/>
+                  <text x="0" y="12">${w / 4}</text>
+                </g>
+                <g x="0" y="0" transform="translate(0,${h / 2})">
+                  <rect x="0" y="0" width="${w / 4}" height="${h / 2}" stroke="lightgray" stroke-width="1" fill="none"/>
+                  <text x="0" y="12">5</text>
+                </g>
+                <g x="0" y="0" transform="translate(${w / 4},${h / 2})">
+                  <rect x="0" y="0" width="${w / 4}" height="${h / 2}" stroke="lightgray" stroke-width="1" fill="none"/>
+                  <text x="0" y="16" font-weight="bold" font-size="16">June Yamamoto 's</text>
+                </g>
+                <g x="0" y="0" transform="translate(${w / 4 * 2},${h / 2})">
+                  <rect x="0" y="0" width="${w / 4}" height="${h / 2}" stroke="lightgray" stroke-width="1" fill="none"/>
+                  <text x="0" y="12">7</text>
+                </g>
+                <g x="0" y="0" transform="translate(${w / 4 * 3},${h / 2})">
+                  <rect x="0" y="0" width="${w / 4}" height="${h / 2}" stroke="lightgray" stroke-width="1" fill="none"/>
+                  <text x="0" y="12">8</text>
+                </g>
+                <rect x="0" y="0" width="5" height="5" />
+                <rect x="775" y="0" width="5" height="5" />
+                <rect x="775" y="${h - 5}" width="5" height="5" />
+                <rect x="0" y="${h - 5}" width="5" height="5" />
+              </g>
+            </svg>`
+          }
         ],
         defaultStyle: {
-          font: 'NotoSans',
+          font: 'FlopDesignFONT',
         },
+        pageSize: 'A4',
+        pageOrientation: 'landscape',
+        pageMargins: [ 30, 30, 30, 30 ],
         compress: false,
         fontLayoutCache: false,
         bufferPages: true
