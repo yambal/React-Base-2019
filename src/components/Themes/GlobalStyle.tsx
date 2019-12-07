5c794f1f2b4b69c08cccdc0d2909f5b3fc80a0b7
June YAMAMOTO <june@yambal.net>
2019-12-07 20:18:23 +0900

    NotoSans, System-UI

--- src/components/Themes/GlobalStyle.tsx
+++ src/components/Themes/GlobalStyle.tsx
@@ -5,6 +5,7 @@ const GlobalStyle = createGlobalStyle`
     font-size: 14px;
   }
   body {
+    font-family: 'Noto Sans JP', system-ui, -apple-system, "Hiragino Sans", "Yu Gothic UI", "Segoe UI", "Meiryo", sans-serif;
     font-size: 1rem;
   }
 `
